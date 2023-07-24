f6975b519c679918fb37370555767e760ef7c512
Abishek R <abisekpillai@gmail.com>
2023-07-25 00:02:51 +0530

    added Constructor and state

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
   render() {
-    let {title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-          <img src="https://static.toiimg.com/thumb/msid-82213159,width-1070,height-580,imgsize-134672,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg" className="card-img-top" alt="..."/>
+          <img src={imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsDetail" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsDetail" className="btn btn-sm btn-primary">Read more</a>
           </div>
         </div>
       </div>
