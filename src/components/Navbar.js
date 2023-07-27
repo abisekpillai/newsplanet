a5ddda95099d392032071abb49be7d0efb6c57ff
Abishek R <abisekpillai@gmail.com>
2023-07-27 18:58:53 +0530

    Previous and Next Pages functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,13 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">NewsPlanet</a>
+            <a className="navbar-brand" href="/">NewsPlanet</a>
+            {/* <a className="navbar-brand" href="/">
+              <img src="newsplanet\public\NewsPlanet.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+            </a> */}
+
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
--- src/components/News.js
+++ src/components/News.js
@@ -7,7 +7,8 @@ export class News extends Component {
     super();
     this.state = {
       articles : [],
-      loading : false
+      loading : false,
+      page : 1
     }
   }
 
@@ -16,7 +17,35 @@ export class News extends Component {
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ articles : parsedData.articles})
+    this.setState({ articles : parsedData.articles, totalResults : parsedData.totalResults})
+  }
+
+  handleNextClick = async () =>{
+    console.log("Next button is clicked");
+    if(this.state.page+1>Math.ceil(this.state.totalResults/12)){
+
+    }
+    else{
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8362708e62074e7cac98b7b12db02254&page=${this.state.page+1}&pageSize=12`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({ articles : parsedData.articles,
+        page : this.state.page+1
+      })
+    }
+  }
+
+  handlePrevClick = async () =>{
+    console.log("Prev button is clicked");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8362708e62074e7cac98b7b12db02254&page=${this.state.page-1}&pageSize=12`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles : parsedData.articles,
+      page : this.state.page-1
+    })
+    
   }
 
   render() {
@@ -24,14 +53,17 @@ export class News extends Component {
     return (
       <div className='container my-3'>
         <h2>NewsPlanet - Top Headlines</h2>
-        {this.state.articles.map((element)=>{console.log(element)})}
         <div className="row">
         {this.state.articles.map((element)=>{
           return <div className="col-md-4" key={element.url}>
                     <NewsItem title={element.title?element.title.slice(0,70):""} description={element.description?element.description.slice(0,105):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
                  </div>
         })}
-        </div> 
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button type="button" disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
       </div>
     )
   }
