e28c3b6cd539d79ba846bcb17975bcb6e3c79d24
Abishek R <abisekpillai@gmail.com>
2023-08-03 13:58:06 +0530

    added categories inside News component

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
-import Spinner from './Spinner';
+import Spinner from './Spinner'
+import PropTypes from 'prop-types'
 
 export class News extends Component {
+
+  static defaultProps = {
+    country : "in",
+    pageSize : 8,
+    category : "general"
+  }
+
+  static propTypes = {
+    country : PropTypes.string,
+    pageSize : PropTypes.number,
+    category : PropTypes.string
+  }
 
   constructor(){
     super();
@@ -14,7 +27,7 @@ export class News extends Component {
   }
 
   async componentDidMount(){
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8362708e62074e7cac98b7b12db02254&page=1&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8362708e62074e7cac98b7b12db02254&page=1&pageSize=${this.props.pageSize}`;
     this.setState({ loading : true});
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -27,7 +40,7 @@ export class News extends Component {
   handleNextClick = async () =>{
     console.log("Next button is clicked");
     if(!(this.state.page+1>Math.ceil(this.state.totalResults/12))){
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8362708e62074e7cac98b7b12db02254&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8362708e62074e7cac98b7b12db02254&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
       this.setState({ loading : true});
       let data = await fetch(url);
       let parsedData = await data.json();
@@ -40,7 +53,7 @@ export class News extends Component {
 
   handlePrevClick = async () =>{
     console.log("Prev button is clicked");
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8362708e62074e7cac98b7b12db02254&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8362708e62074e7cac98b7b12db02254&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
     this.setState({ loading : true});
     let data = await fetch(url);
     let parsedData = await data.json();
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     let {title, description, imageUrl, newsUrl} = this.props;
     return (
       <div>
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card">
           <img src={imageUrl?imageUrl:"https://th.bing.com/th/id/R.ad72b7fecbe0de6526bdde511965ff04?rik=%2bZcPTM8g%2bKiTmQ&riu=http%3a%2f%2fentechdesigns.com%2fnew_site%2fwp-content%2fthemes%2fen-tech%2fimages%2fnot_available_icon.jpg&ehk=UI0frGbrfodEmx3CUtAoVkW7kPtvieYdu3SuVf0niHk%3d&risl=&pid=ImgRaw&r=0"} className="card-img-top" alt="..." width="400" height="200"/>
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
